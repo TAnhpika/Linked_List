c3018c20a5d22795804ecc62056a6f313d157954
TAnhpika <anhpika12345@gmail.com>
2024-08-17 22:04:39 +0700

    v2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Funnycode_TuanAnh");
+        System.out.println("Funnycode_TuanAnh2");
+
     }
 }
